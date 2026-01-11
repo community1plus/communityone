ad6871803c2705c5150bfffa90a82c814ae84da9
community1plus@gmail.com <community1plus@gmail.com>
2026-01-11 12:43:35 +1100

    Initial commit  migrated from noteapp

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from '../components/App';
-import './index.css';
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 
-Amplify.configure(outputs);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
