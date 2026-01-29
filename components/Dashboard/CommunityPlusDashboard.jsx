48387f83b31fa5256c4069cd1b0a44a85423f88c
community1plus@gmail.com <community1plus@gmail.com>
2026-01-29 15:57:39 +1100

    Initial commit  migrated from noteapp

--- components/Dashboard/CommunityPlusDashboard.jsx
+++ components/Dashboard/CommunityPlusDashboard.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
-import CommunityPlusHeader from "./src/components/Header/CommunityPlusHeader";
-import CommunityPlusSidebar from "./src/components/Sidebar/CommunityPlusSidebar";
-import CommunityPlusContentPage from "./src/components/Feed/CommunityPlusContentPage";
+import CommunityPlusHeader from "../Header/CommunityPlusHeader";
+import CommunityPlusSidebar from "../Sidebar/CommunityPlusSidebar";
+import CommunityPlusContentPage from "../Feed/CommunityPlusContentPage";
 import "../src/components/Dashboard/CommunityPlusDashboard.css";
 
 export default function CommunityPlusDashboard({ user, signOut }) {
