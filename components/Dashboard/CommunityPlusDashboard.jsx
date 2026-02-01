e3fad37193df349e19459f717302aad516a1b054
community1plus@gmail.com <community1plus@gmail.com>
2026-02-01 22:32:05 +1100

    Rebuild lockfile using Node 20

--- components/Dashboard/CommunityPlusDashboard.jsx
+++ components/Dashboard/CommunityPlusDashboard.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-
 import CommunityPlusHeader from "../Header/CommunityPlusHeader";
 import CommunityPlusSidebar from "../Sidebar/CommunityPlusSidebar";
 import CommunityPlusContentPage from "../Feed/CommunityPlusContentPage";
