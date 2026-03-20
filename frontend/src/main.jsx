04d329900d0a1204762ec14733e4901f3a580059
community1plus@gmail.com <community1plus@gmail.com>
2026-03-20 16:18:17 +1100

    connect landing page to app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import { Amplify } from "aws-amplify";
+import outputs from "./amplify_outputs.json";
 
-createRoot(document.getElementById('root')).render(
+Amplify.configure(outputs);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
