c2d737b59f29db050e3f764c81814cb29e25539a
community1plus@gmail.com <community1plus@gmail.com>
2026-03-20 16:31:31 +1100

    connect landing page to app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { Amplify } from "aws-amplify";
-import outputs from "./amplify_outputs.json";
 import App from "./App.jsx";
 
 Amplify.configure(outputs);
