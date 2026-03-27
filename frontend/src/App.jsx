07634218aa097c6f074442727c75f7d8bd21e846
community1plus@gmail.com <community1plus@gmail.com>
2026-03-27 21:12:18 +1100

    fix backend package package.json for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import CommunityPlusLandingPage from "./components/CommunityPlusLandingPage/CommunityPlusLandingPage";
 import CommunityPlusYellowPages from "./components/YellowPages/CommunityPlusYellowPages";
-import CommunityPlusDashboard from "./CommunityPlusDashboard";
+import CommunityPlusDashboard from "../components/Dashboard/CommunityPlusDashboard";
 
 export default function App() {
   return (
