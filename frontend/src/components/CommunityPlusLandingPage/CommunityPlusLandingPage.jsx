e3f8c47b946070a34ef8dde5826c29855c45cb1b
community1plus@gmail.com <community1plus@gmail.com>
2026-03-28 08:13:17 +1100

    fix backend package package.json for deployment

--- frontend/src/components/CommunityPlusLandingPage/CommunityPlusLandingPage.jsx
+++ frontend/src/components/CommunityPlusLandingPage/CommunityPlusLandingPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./CommunityPlusLandingPage.css";
 
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "../../AuthContext";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
