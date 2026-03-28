32c89f7e273a3d23c411bb11a4bf221a0b7b1f0f
community1plus@gmail.com <community1plus@gmail.com>
2026-03-28 17:56:01 +1100

    fix backend package package.json for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 
 import CommunityPlusLandingPage from "./components/CommunityPlusLandingPage/CommunityPlusLandingPage";
-import CommunityPlusDashboard from "./components/CommunityPlusDashboard/CommunityPlusDashboard";
+import CommunityPlusDashboard from "./components/Dashboard/CommunityPlusDashboard";
 
 export default function App() {
   const { user, loading } = useAuth();
