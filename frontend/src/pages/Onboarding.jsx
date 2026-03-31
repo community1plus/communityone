8a4c03881205328aa10f91caf755470d004bb6a7
community1plus@gmail.com <community1plus@gmail.com>
2026-04-01 07:35:35 +1100

    fix backend package package.json for deployment

--- frontend/src/pages/Onboarding.jsx
+++ frontend/src/pages/Onboarding.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { apiFetch } from "../..api/services/api";
+import { apiFetch } from "../../services/api";
 
 export default function Onboarding() {
   const [username, setUsername] = useState("");
