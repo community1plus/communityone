6c5b3cecffda794e92cd1a2a7d24eee68d12c621
community1plus@gmail.com <community1plus@gmail.com>
2026-04-01 17:36:37 +1100

    fix backend package package.json for deployment

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,12 @@ import { mockAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/me", mockAuth, getMe);
+router.get("/me", (req, res, next) => {
+  req.user = {
+    userId: "396e64d8-e0c1-70c0-98a2-6758634fccc3", // 👈 your real user
+    email: "test@example.com"
+  };
+  next();
+}, getMe);
 
 export default router;
