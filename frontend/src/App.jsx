345d3fc2e627b7a2d25ffa91c672844754574e4d
community1plus@gmail.com <community1plus@gmail.com>
2026-04-20 02:53:58 +1000

    force rebuild

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -71,7 +71,6 @@ export default function App() {
         }
       >
         <Route path="/home" element={<DashboardHome />} />
-        <Route path="/profile" element={<Onboarding />} />
         <Route path="/profile" element={<CommunityPlusUserProfile />} />
         <Route path="/yellowpages" element={<CommunityPlusYellowPages />} />
         <Route path="/communityplus" element={<CommunityPlusHub />} />
