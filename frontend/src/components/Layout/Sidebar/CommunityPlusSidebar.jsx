ab800af0878eac5bcab765aa6cb06b0f90459bc4
community1plus@gmail.com <community1plus@gmail.com>
2026-04-24 08:11:27 +1000

    force rebuild

--- frontend/src/components/Layout/Sidebar/CommunityPlusSidebar.jsx
+++ frontend/src/components/Layout/Sidebar/CommunityPlusSidebar.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, useLocation } from "react-router-dom";
-import
+import "./CommunityPlusSidebar.css";
 
 export default function CommunityPlusSidebar() {
   const navigate = useNavigate();
