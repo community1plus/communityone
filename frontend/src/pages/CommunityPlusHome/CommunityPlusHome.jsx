3e7d71512f42c5b8bd233d82e465ab29e5c38f78
community1plus@gmail.com <community1plus@gmail.com>
2026-04-26 20:47:15 +1000

    force rebuild

--- frontend/src/pages/CommunityPlusHome/CommunityPlusHome.jsx
+++ frontend/src/pages/CommunityPlusHome/CommunityPlusHome.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./Home.css";
+import "./CommunityPlusHome.css";
 
 import { useMap } from "../../context/MapContext";
 import CommunityMap from "../../components/Map/CommunityMap";
