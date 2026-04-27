a5648680950f44f14034614af64f0fb86bd04689
community1plus@gmail.com <community1plus@gmail.com>
2026-04-27 20:24:21 +1000

    force rebuild

--- frontend/src/pages/CommunityPlusLandingPage/CommunityPlusLandingPage.jsx
+++ frontend/src/pages/CommunityPlusLandingPage/CommunityPlusLandingPage.jsx
@@ -3,7 +3,7 @@ import "./CommunityPlusLandingPage.css";
 
 import { signIn } from "aws-amplify/auth";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 
 export default function CommunityPlusLandingPage() {
   const navigate = useNavigate();
