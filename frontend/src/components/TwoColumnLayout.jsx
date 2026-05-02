bb92a1ccebeb434c0547dc4d9d7d743607d75108
community1plus@gmail.com <community1plus@gmail.com>
2026-05-02 16:10:54 +1000

    force rebuild

--- frontend/src/components/TwoColumnLayout.jsx
+++ frontend/src/components/TwoColumnLayout.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import "./TwoColumnLayout.css";
 
-export default function TwoColumnLayout({ left, right }) {
+export default function TwoColumnLayout({
+  left,
+  right,
+  fullWidth = false,
+  mapMode = false,
+}) {
   return (
-    <div className="two-col-container">
-      <div className="two-col-layout">
+    <div className={`two-col-container ${fullWidth ? "full-width" : ""}`}>
+      <div className={`two-col-layout ${mapMode ? "map-mode" : ""}`}>
+        <div className="two-col-left">{left}</div>
 
-        <div className="two-col-left">
-          {left}
-        </div>
-
-        <div className="two-col-right">
-          {right}
-        </div>
-
+        <div className="two-col-right">{right}</div>
       </div>
     </div>
   );
