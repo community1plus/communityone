87995c82ffdcec87ebff395992f9e29111683028
community1plus@gmail.com <community1plus@gmail.com>
2026-05-13 22:45:47 +1000

    Fix S3 presigned upload signature

--- frontend/src/pages/CommunityPlusIViewPage/CommunityPlusIViewPage.jsx
+++ frontend/src/pages/CommunityPlusIViewPage/CommunityPlusIViewPage.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import "./CommunityPlusIViewPage.css";
-
-import CommunityPlusAdTv from "../../components/CommunityPlusAdTv/CommunityPlusAdTv";
+import CommunityPlusAdTv from "./CommunityPlusAdTv/CommunityPlusAdTv";
 
 const API_BASE =
   import.meta.env.VITE_API_BASE ||
