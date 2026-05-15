ddf3330efc8e9b3d269f48535cb596f178f65640
community1plus@gmail.com <community1plus@gmail.com>
2026-05-15 23:13:31 +1000

    Add signed media URLs to posts feed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ import CommunityPlusDashboardHome from "./pages/CommunityPlusDashboardHome/Commu
 import CommunityPlusYellowPages from "./pages/CommunityPlusYellowPages/CommunityPlusYellowPages";
 import CommunityPlusUserProfile from "./pages/CommunityPlusUserProfile/CommunityPlusUserProfile";
 import CommunityPlusIViewPage from "./pages/CommunityPlusIViewPage/CommunityPlusIViewPage";
-import CommunityPlusChannels from "./pages/communityPlusChannels/communityPlusChannels";
+import CommunityPlusChannels from "./pages/CommunityPlusChannels/communityPlusChannels";
 import PostComposer from "./pages/CommunityPlusAdTv/CommunityPlusAdTv"
 import CommunityPlusAdTv from "./pages/CommunityPlusAdTv/CommunityPlusAdTv";
 
