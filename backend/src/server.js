dcba0117770ab300d70adca5c258bb2ed38f4661
community1plus@gmail.com <community1plus@gmail.com>
2026-06-16 14:29:54 +1000

    Push to development - cors fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -98,10 +98,16 @@ process.on("unhandledRejection", (err) => {
 app.use(
   cors({
     origin: [
-      "https://main.d1ss8rtrtimogr.amplifyapp.com",
-      "http://localhost:5173",
-      "https://www.comm-unity.one",
-      "https://comm-unity.one"
+"http://localhost:5173",
+  "http://localhost:5173/",
+  "https://main.d1ss8rtrtimogr.amplifyapp.com",
+  "https://main.d1ss8rtrtimogr.amplifyapp.com/",
+  "https://develop.d1ss8rtrtimogr.amplifyapp.com",
+  "https://develop.d1ss8rtrtimogr.amplifyapp.com/",
+  "https://www.comm-unity.one",
+  "https://www.comm-unity.one/",
+  "https://comm-unity.one",
+  "https://comm-unity.one/",
     ],
 
     credentials: true,
