2fdfd37dc2a9b28a9ea3f0bf699f981d3a346e5f
community1plus@gmail.com <community1plus@gmail.com>
2026-06-21 00:40:36 +1000

    Push to development - cors fix

---
+++ frontend/src/context/useUserLocation.jsx
@@ -0,0 +1,12 @@
+import { useLocation } from "../context/LocationProvider";
+
+export default function useUserLocation() {
+  const location = useLocation();
+
+  return {
+    homeLocation: location?.homeLocation || null,
+    liveLocation: location?.liveLocation || null,
+    viewLocation: location?.viewLocation || null,
+    loading: location?.loading || false,
+  };
+}
