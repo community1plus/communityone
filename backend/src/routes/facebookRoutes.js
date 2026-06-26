0943da3267183e69a6fad48fc336ed34542583b9
community1plus@gmail.com <community1plus@gmail.com>
2026-06-26 11:02:51 +1000

    Push to development - cors fix

--- backend/src/routes/facebookRoutes.js
+++ backend/src/routes/facebookRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import crypto from "crypto";
 import authMiddleware from "../../middleware/authMiddleware.js";
-import { saveProfile } from "../controllers/profileController.js";
+import saveProfile from "../../controllers/profileController.js";
 
 const router = express.Router();
 
