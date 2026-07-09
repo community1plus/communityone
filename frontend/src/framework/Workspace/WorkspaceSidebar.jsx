10aff7e14ab96df726032d137b5934538fc45416
community1plus@gmail.com <community1plus@gmail.com>
2026-07-10 04:44:42 +1000

    Push to development

--- frontend/src/framework/Workspace/WorkspaceSidebar.jsx
+++ frontend/src/framework/Workspace/WorkspaceSidebar.jsx
@@ -1,11 +1,13 @@
 export default function WorkspaceSidebar({ children }) {
+
     return (
+
         <aside className="workspace-sidebar">
 
-            <div className="workspace-sidebar-inner">
-                {children}
-            </div>
+            {children}
 
         </aside>
+
     );
+
 }
