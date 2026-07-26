4367cdef1969ff7e4898a55c129e1394eea26e74
community1plus@gmail.com <community1plus@gmail.com>
2026-07-26 22:55:32 +1000

    Push to development

--- frontend/src/engines/IdentityWorkspace/sections/IdentitySectionRenderer.jsx
+++ frontend/src/engines/IdentityWorkspace/sections/IdentitySectionRenderer.jsx
@@ -5,8 +5,8 @@ import WorkspaceForm from "../../../framework/Workspace/Form/WorkspaceForm";
 
 import FormBuilder from "../../../components/UI/Form/FormBuilder";
 
-import IdentitySocialSection from "../components/IdentitySocialSection";
-import IdentityPaymentSection from "../components/IdentityPaymentSection";
+import IdentitySocialSection from "../../components/IdentitySocialSection";
+import IdentityPaymentSection from "../../components/IdentityPaymentSection";
 
 export default function IdentitySectionRenderer({
 
