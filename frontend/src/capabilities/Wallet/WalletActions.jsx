04b4f20f0e7cdecc0b39f70bb913750cbcd16240
community1plus@gmail.com <community1plus@gmail.com>
2026-07-28 23:33:01 +1000

    Push to development

---
+++ frontend/src/capabilities/Wallet/WalletActions.jsx
@@ -0,0 +1,43 @@
+import { Button } from "../../framework/Button";
+
+export default function WalletActions({
+
+    onTransfer,
+    onDeposit,
+    onWithdraw,
+
+}) {
+
+    return (
+
+        <>
+
+            <Button
+                fullWidth
+                variant="primary"
+                onClick={onTransfer}
+            >
+                Transfer
+            </Button>
+
+            <Button
+                fullWidth
+                variant="secondary"
+                onClick={onDeposit}
+            >
+                Deposit
+            </Button>
+
+            <Button
+                fullWidth
+                variant="secondary"
+                onClick={onWithdraw}
+            >
+                Withdraw
+            </Button>
+
+        </>
+
+    );
+
+}
