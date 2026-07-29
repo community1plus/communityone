bedd93c95f1c7523eca6bdecc7509e83773ac6df
community1plus@gmail.com <community1plus@gmail.com>
2026-07-29 23:34:49 +1000

    Push to development

--- frontend/src/experience/PersonalExperience/PersonalExperience.jsx
+++ frontend/src/experience/PersonalExperience/PersonalExperience.jsx
@@ -1,38 +1,15 @@
-import { useState } from "react";
-
-import WalletWorkspace from "../../capabilities/Wallet/WalletWorkspace";
-import { useState } from "react";
-
 import IdentityWorkspace from "../../engines/IdentityWorkspace/IdentityWorkspace";
 
-
 export default function PersonalExperience({
-
     workspaceState,
     workspaceActions,
-
 }) {
 
-    const [capability] =
-        useState("identity");
-
-    switch (capability) {
-
-        case "wallet":
-
-            return (
-                <WalletWorkspace />
-            );
-
-        default:
-
-            return (
-                <IdentityWorkspace
-                    state={workspaceState}
-                    actions={workspaceActions}
-                />
-            );
-
-    }
+    return (
+        <IdentityWorkspace
+            state={workspaceState}
+            actions={workspaceActions}
+        />
+    );
 
 }
