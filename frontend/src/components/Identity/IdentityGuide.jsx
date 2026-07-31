3a787adb5dc0d2a17afcc2c36cba00c30343d916
community1plus@gmail.com <community1plus@gmail.com>
2026-07-31 16:00:13 +1000

    Push to development

--- frontend/src/components/Identity/IdentityGuide.jsx
+++ frontend/src/components/Identity/IdentityGuide.jsx
@@ -2,6 +2,7 @@ import GuideCardProgress from "../Guide/GuideCardProgress";
 import GuideCardAI from "../Guide/GuideCardAI";
 import GuideCardHelp from "../Guide/GuideCardHelp";
 import GuideCardNext from "../Guide/GuideCardNext";
+import GuideCardTips from "../Guide/GuideCardTips";
 
 export default function IdentityGuide({
 
--- frontend/src/engines/IdentityWorkspace/IdentityWorkspace.jsx
+++ frontend/src/engines/IdentityWorkspace/IdentityWorkspace.jsx
@@ -24,7 +24,7 @@ import {
 } from "../../framework/Workspace";
 
 import IdentityActions from "../../components/Identity/IdentityActions";
-import IdentityHelpPanel from "../../components/Guide/GuideCardHelp";
+import IdentityGuide from "../../components/Identity/IdentityGuide";
 import IdentityCapabilitySelector from "../../components/Identity/IdentityCapabilitySelector";
 import IdentitySectionRenderer from "../IdentityWorkspace/sections/IdentitySectionRenderer";
 
