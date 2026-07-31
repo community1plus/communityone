095eb1103b56b9bce2d4383e760dc3f6d0a01578
community1plus@gmail.com <community1plus@gmail.com>
2026-08-01 09:35:22 +1000

    Push to development

---
+++ frontend/src/components/Identity/Progress/ProgressRing.jsx
@@ -0,0 +1,23 @@
+export default function ProgressRing({ model }) {
+
+    return (
+
+        <div className="progress-ring">
+
+            <div className="progress-ring-circle">
+
+                {model.percentage}%
+
+            </div>
+
+            <div className="progress-ring-label">
+
+                {model.label}
+
+            </div>
+
+        </div>
+
+    );
+
+}
