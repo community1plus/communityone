122f0766f694e390cdfd396e30d812d08f6f1576
community1plus@gmail.com <community1plus@gmail.com>
2026-08-01 12:03:42 +1000

    Push to development

--- frontend/src/framework/Workspace/models/WorkspaceHeaderModel.js
+++ frontend/src/framework/Workspace/models/WorkspaceHeaderModel.js
@@ -1,11 +1,23 @@
 export function createWorkspaceHeaderModel({
+
     title,
     subtitle,
+
+    meta,
+
     onClose,
+
 }) {
+
     return {
+
         title,
         subtitle,
+
+        meta,
+
         onClose,
+
     };
+
 }
