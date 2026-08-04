fc0dca39ebbdcb29c22c760c79f3e6ebf271d6b6
community1plus@gmail.com <community1plus@gmail.com>
2026-08-04 23:33:24 +1000

    Push to development

--- frontend/src/framework/Workspace/WorkspaceContext.jsx
+++ frontend/src/framework/Workspace/WorkspaceContext.jsx
@@ -8,29 +8,27 @@ export default function WorkspaceContext({
 
     return (
 
-<section className="workspace-context">
+        <section className="workspace-context">
 
-    <div className="workspace-context-mode">
+            <div className="workspace-context-mode">
 
-        {mode}
+                {mode}
 
-    </div>
+            </div>
 
-    <div className="workspace-context-spacer" />
+            <div className="workspace-context-meta">
 
-    <div className="workspace-context-meta">
+                {meta}
 
-        {meta}
+            </div>
 
-    </div>
+            <div className="workspace-context-actions">
 
-    <div className="workspace-context-actions">
+                {actions}
 
-        {actions}
+            </div>
 
-    </div>
-
-</section>
+        </section>
 
     );
 
