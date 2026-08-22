3b6c9beab69cfe82dd6f33251cafa86728b7be24
community1plus@gmail.com <community1plus@gmail.com>
2026-08-22 23:40:05 +1000

    Push to development

--- frontend/src/framework/Workspace/index.js
+++ frontend/src/framework/Workspace/index.js
@@ -78,6 +78,9 @@ from "./Card/WorkspaceCardBody";
 export { default as WorkspaceCardFooter }
 from "./Card/WorkspaceCardFooter";
 
+export {
+    default as WorkspaceSectionHeader,
+} from "../Workspace/Regions/WorkspaceSectionHeader";
 
 export { default as WorkspacePanel }
 from "./WorkspacePanel";
