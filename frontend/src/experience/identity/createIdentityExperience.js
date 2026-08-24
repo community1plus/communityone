e2f35d6317aaf8967df0cafa5eda35ce3cd90e47
community1plus@gmail.com <community1plus@gmail.com>
2026-08-25 03:52:28 +1000

    Push to development

--- frontend/src/experience/identity/createIdentityExperience.js
+++ frontend/src/experience/identity/createIdentityExperience.js
@@ -26,7 +26,7 @@ export function createIdentityExperience({
 
             header: Workspace.Header({
 
-                title: "IDENTITY",
+                title: "PROFILE",
 
                 subtitle: "Your trusted identity.",
 
