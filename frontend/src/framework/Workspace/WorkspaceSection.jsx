800e9271f1f9f489356b52dffd8fb0476a01675e
community1plus@gmail.com <community1plus@gmail.com>
2026-08-26 18:09:40 +1000

    Push to development

--- frontend/src/framework/Workspace/WorkspaceSection.jsx
+++ frontend/src/framework/Workspace/WorkspaceSection.jsx
@@ -75,8 +75,8 @@ export default function WorkspaceSection({
                 ================================= */}
 
                 <WorkspaceSectionActions
-                    actions={actions}
-                />
+    actions={["edit"]}
+/>
 
 
             </header>
