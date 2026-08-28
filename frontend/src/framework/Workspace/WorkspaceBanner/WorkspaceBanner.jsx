cd781ff6a13319661cd454f182879f9011816e74
community1plus@gmail.com <community1plus@gmail.com>
2026-08-28 17:48:06 +1000

    Push to development

--- frontend/src/framework/Workspace/WorkspaceBanner/WorkspaceBanner.jsx
+++ frontend/src/framework/Workspace/WorkspaceBanner/WorkspaceBanner.jsx
@@ -4,29 +4,169 @@
    Workspace Banner
    ========================================================== */
 
+import WorkspaceBannerSection
+    from "../WorkspaceBannerSection/WorkspaceBannerSection";
+
+import WorkspaceTitle
+    from "../WorkspaceTitle";
+
+import WorkspaceMode
+    from "../WorkspaceMode";
+
+import WorkspaceMetric
+    from "../WorkspaceMetric";
+
+import "./WorkspaceBanner.css";
+
+
 export default function WorkspaceBanner({
 
+    model = {},
+
     left = null,
+
     center = null,
+
     right = null,
 
+    children,
+
 }) {
+
+
+    /* =========================================
+       MODEL
+    ========================================= */
+
+    const {
+
+        left: modelLeft = {},
+
+        centre: modelCentre = {},
+
+        right: modelRight = {},
+
+    } = model;
+
+
+    /* =========================================
+       LEFT
+    ========================================= */
+
+    const leftContent =
+
+        left
+
+        ??
+
+        (
+
+            <WorkspaceBannerSection>
+
+                <WorkspaceTitle
+                    title={
+                        modelLeft.title
+                    }
+                />
+
+            </WorkspaceBannerSection>
+
+        );
+
+
+    /* =========================================
+       CENTER
+    ========================================= */
+
+    const centerContent =
+
+        center
+
+        ??
+
+        (
+
+            <WorkspaceBannerSection>
+
+                <WorkspaceMode>
+
+                    {children}
+
+                </WorkspaceMode>
+
+            </WorkspaceBannerSection>
+
+        );
+
+
+    /* =========================================
+       RIGHT
+    ========================================= */
+
+    const rightContent =
+
+        right
+
+        ??
+
+        (
+
+            <WorkspaceBannerSection>
+
+                <WorkspaceMetric
+
+                    model={
+                        modelRight.metric
+                    }
+
+                />
+
+            </WorkspaceBannerSection>
+
+        );
+
+
+    /* =========================================
+       RENDER
+    ========================================= */
 
     return (
 
         <div className="workspace-banner">
 
+
+            {/* =================================
+               LEFT
+            ================================= */}
+
             <div className="workspace-banner-left">
-                {left}
+
+                {leftContent}
+
             </div>
+
+
+            {/* =================================
+               CENTER
+            ================================= */}
 
             <div className="workspace-banner-center">
-                {center}
+
+                {centerContent}
+
             </div>
 
+
+            {/* =================================
+               RIGHT
+            ================================= */}
+
             <div className="workspace-banner-right">
-                {right}
+
+                {rightContent}
+
             </div>
+
 
         </div>
 
