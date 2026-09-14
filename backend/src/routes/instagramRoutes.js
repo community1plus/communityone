03328e40267cd1efe9f2965f7807eb0e6ef87468
community1plus@gmail.com <community1plus@gmail.com>
2026-09-14 19:07:46 +1000

    Push to development

--- backend/src/routes/instagramRoutes.js
+++ backend/src/routes/instagramRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import crypto from "crypto";
 
-import authMiddleware from "../middleware/authMiddleware.js";
+import authMiddleware from "../../middleware/authMiddleware.js";
 import { patchProfileService } from "../services/profileService.js";
 
 const router = express.Router();
